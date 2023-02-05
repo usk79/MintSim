41baedc266cd03639e15f03984734298db2a2c26
usk79 <yusuke.m79@outlook.jp>
2023-02-05 11:04:35 +0900

    RampFuncの実装

--- src/simcore/sim_model/source_models.rs
+++ src/simcore/sim_model/source_models.rs
@@ -83,13 +83,13 @@ impl ModelCore for ConstantFunc {
 #[derive(Debug)]
 pub struct StepFunc {
     outbus: Bus,
-    settings: Vec<(f64, f64, f64)>, // (step_time, init_value, final_value)
+    settings: Vec<(f64, f64, f64)>,
 }
 
 impl StepFunc {
     /// ## StepFuncの引数定義
     /// 1. 第1引数：Bus
-    /// 1. 第2引数：settings: Vec<(step_time, init_value, final_value)>
+    /// 1. 第2引数：settings: Vec<(init_value, final_value, step_time)>
     /// ## 注意事項
     /// Busの要素数とsettingsの要素数は等しい必要があります。
     pub fn new(outbus: Bus, settings: Vec<(f64, f64, f64)>) -> anyhow::Result<Self> {
@@ -107,7 +107,7 @@ impl StepFunc {
 impl ModelCore for StepFunc {
     fn initialize(&mut self, _sim_time: &SimTime) {
         self.outbus.iter_mut().enumerate().for_each(|(idx, sig)| {
-            sig.set_val(self.settings[idx].1)
+            sig.set_val(self.settings[idx].0)
         });
     }
 
@@ -118,8 +118,8 @@ impl ModelCore for StepFunc {
     fn nextstate(&mut self, sim_time: &SimTime) {
         self.outbus.iter_mut().enumerate().for_each(|(idx, sig)| {
             let set = self.settings[idx];
-            if sim_time.time() >= set.0 {
-                sig.set_val(set.2);
+            if sim_time.time() >= set.2 {
+                sig.set_val(set.1);
             }
         });
     }
@@ -138,29 +138,32 @@ impl ModelCore for StepFunc {
 #[derive(Debug)]
 struct RampFunc {
     outbus: Bus, 
-    start_time: f64,    // 増加開始時刻
-    slope: f64,         // 1秒あたりの傾き
-    init_value: f64,    // 初期値
-    final_value: f64,   // 最終値
-    limit_enable: bool, // 最終値の有効/無効　有効時はfinal_valueで頭打ち、無効時は上限なしに増加を継続
+    settings: Vec<(f64, f64, bool, f64, f64)>, // Vec<(init_value, final_value, start_time, slope, limit_enable)>
 }
 
 impl RampFunc {
-    pub fn new(outbus: Bus, start_time: f64, slope: f64, init_value: f64, final_value: f64, limit_enable: bool) -> Self {
-        Self {
-            outbus: outbus,
-            start_time: start_time,
-            slope: slope,
-            init_value: init_value,
-            final_value: final_value,
-            limit_enable: limit_enable,
+    /// ## RampFuncの引数定義
+    /// 1. 第1引数：Bus
+    /// 1. 第2引数：settings: Vec<(init_value, limit_value, limit_enable, start_time, slope)>
+    /// ## 注意事項
+    /// Busの要素数とsettingsの要素数は等しい必要があります。
+    pub fn new(outbus: Bus, settings: Vec<(f64, f64, bool, f64, f64)>) -> anyhow::Result<Self> {
+        if outbus.len() != settings.len() {
+            return Err(anyhow!("outbusとsettingsの要素数は一致している必要があります。\noutbus.len = {}, settings.len = {} ", outbus.len(), settings.len()))
         }
+
+        Ok(Self {
+            outbus: outbus,
+            settings: settings,
+        })
     }
 }
 
 impl ModelCore for RampFunc {
     fn initialize(&mut self, _sim_time: &SimTime) {
-        self.outbus.set_all(self.init_value);
+        self.outbus.iter_mut().enumerate().for_each(|(idx, sig)| {
+            sig.set_val(self.settings[idx].0)
+        });
     }
 
     fn finalize(&mut self) {
@@ -168,11 +171,19 @@ impl ModelCore for RampFunc {
     }
 
     fn nextstate(&mut self, sim_time: &SimTime) {
-        if sim_time.time() >= self.start_time {
-            let delta = self.slope * sim_time.delta_t(); // 1ステップ当たりの傾きを求める
-            let val = self.outbus[0].val() + delta;
-            self.outbus.set_all(val)
-        }
+        self.outbus.iter_mut().enumerate().for_each(|(idx, sig)| {
+            let set = self.settings[idx];
+            let delta = set.4 * sim_time.delta_t(); // 1ステップ当たりの増加量
+            if sim_time.time() >= set.3 {
+                let mut val = sig.val() + delta;
+                if set.2 == true && delta >= 0.0 { // 上限が有効であれば上限を設定する
+                    val = val.min(set.1);
+                } else if set.2 == true && delta < 0.0 {
+                    val = val.max(set.1);
+                }
+                sig.set_val(val);
+            }
+        });
     }
 
     fn interface_in(&mut self) -> Option<&mut RefBus> {
@@ -255,40 +266,9 @@ mod source_model_test {
             (500, 500),
             (2, 1)
         ).unwrap();
-    }
-/*
-    #[test]
-    fn step_func_test_old() {
-        
-        let stbus = Bus::try_from(vec![
-            SigDef::new("step", "-"),
-        ]).unwrap();
-
-        let st = StepFunc::new(stbus, 0.3, 0.2, 0.5);
-        
-        let mut scope_bus = RefBus::try_from(vec![
-            SigDef::new("step", "-"),
-        ]).unwrap();
-
-        scope_bus.connect_to(st.interface_out().unwrap(), &["step"], &["step"]).unwrap();
-        let scope = SimRecorder::new(scope_bus);
-
-        let mut sys = SimSystem::new(0.0, 1.0, 0.01);
-        
-        sys.regist_model(st);
-        sys.regist_recorder("rcd1", scope);
-        
-        sys.run(); // sysに名前から中のモデルにアクセスできるようにハッシュマップをもたせる
-
-        sys.get_recorder("rcd1").unwrap().timeplot_all(
-            "test_output\\step_func.png", 
-            (500, 500),
-            (1, 1)
-        ).unwrap();
-        
 
     }
-*/
+
     #[test]
     #[should_panic]
     fn step_func_panic_test() {
@@ -301,6 +281,68 @@ mod source_model_test {
                 (0.5, 0.0, 1.0),
                 (0.3, 1.0, 1.5),
                 (0.3, 1.0, 1.5)]
+        ).unwrap();
+    }
+
+    #[test]
+    fn ramp_func_test() {
+        let sf = RampFunc::new(
+            Bus::try_from(vec![
+                SigDef::new("rf1", "Nm"),
+                SigDef::new("rf2", "A"),
+                SigDef::new("rf3", "Nm"),
+                SigDef::new("rf4", "A"),
+            ]).unwrap(),
+            vec![
+                (0.5, 1.5, true, 0.2, 2.0),
+                (0.5, 0.0, false, 0.3, 2.0),
+                (0.5, -1.5, true, 0.3, -2.0),
+                (0.5, 0.0, false, 0.3, -2.0),
+            ]
+        ).unwrap();
+
+        let mut scp = SimRecorder::new(
+            RefBus::try_from(vec![
+                SigDef::new("Scp_rf1", "Nm"),
+                SigDef::new("Scp_rf2", "A"),
+                SigDef::new("Scp_rf3", "Nm"),
+                SigDef::new("Scp_rf4", "A"),
+            ]).unwrap()
+        );
+
+        scp.interface_in().unwrap().connect_to(sf.interface_out().unwrap(), 
+            &["rf1", "rf2", "rf3", "rf4"], 
+            &["Scp_rf1", "Scp_rf2", "Scp_rf3", "Scp_rf4"],
+        ).unwrap();
+
+        let mut sys = SimSystem::new(0.0, 1.0, 0.01);
+
+        sys.regist_model(sf);
+        sys.regist_recorder("scp1", scp);
+
+        sys.run();
+
+        sys.get_recorder("scp1").unwrap().timeplot_all(
+            "test_output\\ramp_func.png", 
+            (500, 500),
+            (2, 2)
+        ).unwrap();
+
+    }
+
+    #[test]
+    #[should_panic]
+    fn ramp_func_panic_test() {
+        let _sf = RampFunc::new(
+            Bus::try_from(vec![
+                SigDef::new("Rf1", "Nm"),
+                SigDef::new("Rf2", "A"),
+            ]).unwrap(),
+            vec![
+                (0.5, 1.5, true, 0.2, 2.0),
+                (0.5, 0.0, false, 0.3, 2.0),
+                (0.5, -1.5, true, 0.3, -2.0),
+                (0.5, 0.0, false, 0.3, -2.0),]
         ).unwrap();
     }
 }
